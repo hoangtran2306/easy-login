3805ee4acdb4ed1be5d2ec2e46da47932e23b402
hoangta <hoangta@ai-t.vn>
2018-03-30 10:48:10 +0700

    fix callback url

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '1441533179394758', // your App ID
         'clientSecret'    : '10df5ddc29567e9d440de0e996dd5f82', // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'     : '/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.9/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     },
